c256de76a5f5ab1a1e199b3bbfa16ece4d5b7166
Sachin Jadhav <sachinjadhav@Sachins-MacBook-Pro.local>
2023-03-30 14:17:31 +0530

    Remove console log

--- index.js
+++ index.js
@@ -26,7 +26,6 @@
 // const wss = new WebSocket.Server({ server: server });
 
 // wss.on("connection", function connection(ws) {
-//   console.log("A new client Connected!");
 //   ws.send("Welcome New Client!");
 
 //   ws.on("message", function incoming(message) {

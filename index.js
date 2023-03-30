4109ba2e8784ff65ca8cfc3a4806a334849801b2
Sachin Jadhav <sachinjadhav@Sachins-MacBook-Pro.local>
2023-03-30 13:46:15 +0530

    Use express with ws

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var { WebSocketServer } = require("ws");
+// var { WebSocketServer } = require("ws");
 
 const GEOMETRY_MINIMUM_SIZE = 0.75;
 const GEOMETRY_MAXIMUM_SIZE = 1.25;
 
-const wss = new WebSocketServer({ port: 8080 });
+// const wss = new WebSocketServer({ port: 8080 });
 
 const randomNumberGenerator = () => {
   return (
@@ -12,8 +12,28 @@ const randomNumberGenerator = () => {
   ).toFixed(3);
 };
 
+// wss.on("connection", function connection(ws) {
+//   ws.on("message", function message(_data) {
+//     ws.send(randomNumberGenerator());
+//   });
+// });
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ server: server });
+
 wss.on("connection", function connection(ws) {
-  ws.on("message", function message(_data) {
+  console.log("A new client Connected!");
+  ws.send("Welcome New Client!");
+
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
     ws.send(randomNumberGenerator());
   });
 });
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+server.listen(3000, () => console.log(`Lisening on port :3000`));
